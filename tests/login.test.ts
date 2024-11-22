2d09dc6bafbfd970af911523f59f248c04796dee
MalinPettersson36-hub <malin.pettersson@elevmail.stockholm.se>
2024-11-22 16:53:59 +0100

    password

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -2,13 +2,17 @@ import test, { expect } from "@playwright/test"
 import { LoginPage } from "../pages/loginpage"
 import { StorePage } from "../pages/storepage"
 
+let password: string
 test('Login with Markus', async ({page}) => {
     const loginPage = new LoginPage(page)
     const storePage = new StorePage(page)
-
+    if (process.env.PASSWORD !== undefined){
+        password = process.env.PASSWORD
+    }
+    
 
     await page.goto("http://hoff.is/login")
-    loginPage.login("Markus","sup3rs3cr3t", 'consumer')
+    loginPage.login("Markus",password, 'consumer')
     //const username = await storePage.usernameText.textContent()
     const header = await storePage.header.textContent()
 
