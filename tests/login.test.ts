5cadaf6eef4b1c9aaf5ac0e77ab84a0feb865cf0
MalinPettersson36-hub <malin.pettersson@elevmail.stockholm.se>
2024-12-09 12:40:58 +0100

    Lite fix

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,4 +1,4 @@
-import test, { expect } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import { LoginPage } from "../pages/loginpage"
 import { StorePage } from "../pages/storepage"
 
@@ -6,16 +6,17 @@ let password: string
 test('Login with Markus and verify Storepage can be reached', async ({page}) => {
     const loginPage = new LoginPage(page)
     const storePage = new StorePage(page)
-    /*if (process.env.PASSWORD !== undefined){
-        password = process.env.PASSWORD
-    }*/
     
-
+    if (process.env.PASSWORD !== undefined){
+        password = process.env.PASSWORD
+    }
+    
     await page.goto("http://hoff.is/login")
+    //Password blev undefined. Fick inte miljövariabeln att fungera.
     loginPage.login("Markus","sup3rs3cr3t", 'consumer')
-    //const username = await storePage.usernameText.textContent()
+    
     const header = await storePage.header.textContent()
-
-    //expect(username).toBe("markus")
+    
     expect(header).toBe("Store")
+    
 })
